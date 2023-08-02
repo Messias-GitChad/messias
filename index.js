37e1e5e49759faacc9ef8e19a1b1dedf121a08e0
Jonas Leite <jaycepain@gmail.com>
2023-08-02 16:41:27 -0300

    -Jonas Commit

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-// segundo commit
-//commit de teste
-// commit 3
-//Nicolas-Commit-4
-// Bea fazendo commit 
-<<<<<<< HEAD
+
 // segundo commit 
 
 
 //terceiro
 
 
-//commit -Jonas
-=======
-//Commit-Oziel :)
->>>>>>> af97b73d463132b5fdd1fbee04bcab115d5cd132
+//commit -Jonas// segundo commit
+//commit de teste
+// commit 3
+//Nicolas-Commit-4
+// Bea fazendo commit 
